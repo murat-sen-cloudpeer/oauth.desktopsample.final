f95896f24894da1ef9d3b9b9b8f7c9c3b62de570
gary <gary@MACSTATION.local>
2021-10-31 11:31:58 +0000

    Updates

--- src/views/userInfo/userInfoView.tsx
+++ src/views/userInfo/userInfoView.tsx
@@ -99,7 +99,7 @@ export function UserInfoView(props: UserInfoViewProps): JSX.Element {
 
         const options = {
             reload,
-            isInLoggedOutView: RouteHelper.isInLoginRequiredView(),
+            isInMainView: RouteHelper.isInMainView(),
             causeError,
         };
 
