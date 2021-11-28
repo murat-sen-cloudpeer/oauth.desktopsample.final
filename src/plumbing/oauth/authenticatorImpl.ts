1cc4844a4de4a09e52ff905c0c7f982b239ee7fe
Gary Archer <32388530+gary-archer@users.noreply.github.com>
2021-11-28 16:41:30 +0000

    Update authenticatorImpl.ts

--- src/plumbing/oauth/authenticatorImpl.ts
+++ src/plumbing/oauth/authenticatorImpl.ts
@@ -186,7 +186,7 @@ export class AuthenticatorImpl implements Authenticator {
     public async expireRefreshToken(): Promise<void> {
 
         if (this._tokens && this._tokens.refreshToken) {
-            this._tokens.refreshToken = `${this._tokens.refreshToken}x`
+            this._tokens.refreshToken = `${this._tokens.refreshToken}x`;
             this._tokens.accessToken = null;
             await this._events.saveTokens(this._tokens);
         }
